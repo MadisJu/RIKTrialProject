523208a128128a1b716586aacdbbb1e924a3266e
Madis-Julius <estonira@gmail.com>
2026-01-27 17:19:05 +0200

    Added basic datamodels with navigational properties.

---
+++ RIKTrialServer/Models/Data/PaymentMethod.cs
@@ -0,0 +1,8 @@
+﻿namespace RIKTrialServer.Models.Data
+{
+    public class PaymentMethod
+    {
+        public int Id { get; set; }
+        public string Method { get; set; } = null!;
+    }
+}
---
+++ RIKTrialServer/Models/Data/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace RIKTrialServer.Models.Data
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public int ParticipantId { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string IdNumber { get; set; } = null!;
+        public string AdditionalInfo { get; set; } = null!;
+
+        // -- ef navigation --
+
+        public Participant Participant { get; set; } = null!;
+    }
+}
